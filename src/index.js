c02bc4693eb733c808013d3ee03106165e86d226
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-09-28 23:02:34 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Your code to handle the form submission can go here
+    // For example, you can access the task description from the input field:
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    // You can then use the task description to add a new task to the list
+    // For simplicity, let's just alert the task description for now:
+    alert(`New task created: ${taskDescription}`);
+  });
 });
+
